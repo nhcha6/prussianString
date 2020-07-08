e60c82ca2242f96e2fa168c52610d9f09976b2a9
nhcha6 <nhcha6@student.monash.edu>
2020-07-08 16:46:53 +1000

    data colour coded into based on village/manor/town, and a very small amount of noise added to distinguish between identical matches

--- Code/fraustadt_map_generation.py
+++ Code/fraustadt_map_generation.py
@@ -1,5 +1,5 @@
 # file for producing a map of fraustadt data
-
+import numpy as np
 import pandas as pd
 import geopandas as gpd
 import geoplot as gplt
@@ -15,13 +15,21 @@ prussia_map = prussia_map.to_crs(epsg=4326)
 
 # read in merged data
 fraustadt_merged_df = pd.read_excel(wdir+"PrussianCensus1871/Fraustadt/Posen-Fraustadt-kreiskey-134-merged.xlsx")
+
+# we only want entries with long-lat data
 fraustadt_merged_df = fraustadt_merged_df[fraustadt_merged_df['lat']!=0]
+# !! find fraustadt geometric object and check a point is within.
+# if there are multiple matches, simply take the second for now. !! still need better duplicate distinction.
+fraustadt_merged_df = fraustadt_merged_df.drop_duplicates(subset=['loc_id'], keep='last')
+# add a little bit of noise to ensure that identical data points are split slightly
+fraustadt_merged_df['lat'] = np.random.normal(fraustadt_merged_df['lat'],0.01)
+fraustadt_merged_df['lng'] = np.random.normal(fraustadt_merged_df['lng'],0.01)
+
+
 fraustadt_merged_gdf = gpd.GeoDataFrame(fraustadt_merged_df, geometry=gpd.points_from_xy(fraustadt_merged_df.lng,fraustadt_merged_df.lat))
 
-# plot prussia base map
-prussia_plot = gplt.polyplot(prussia_map, figsize=(8,4))
-gplt.pointplot(fraustadt_merged_gdf, ax=prussia_plot)
-fraustadt_plot = gplt.pointplot(fraustadt_merged_gdf)
+# plot prussia base map and fraustadt points
+fraustadt_plot = gplt.pointplot(fraustadt_merged_gdf, hue='class', legend=True)
 gplt.polyplot(prussia_map, ax=fraustadt_plot)
 plt.show()
 
