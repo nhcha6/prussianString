de25199bcbaa75fb52d8bb048596d3334940046f
nhcha6 <nhcha6@student.monash.edu>
2020-07-08 15:47:41 +1000

    longitude and lattitude data derived from the meyers gazetter plotted over map of prussia. Data looks to be a good fit

--- Code/fraustadt_map_generation.py
+++ Code/fraustadt_map_generation.py
@@ -1,1 +1,27 @@
-# file for producing a map of fraustadt
+# file for producing a map of fraustadt data
+
+import pandas as pd
+import geopandas as gpd
+import geoplot as gplt
+import matplotlib.pyplot as plt
+
+# set working directory path as location of data
+wdir = '/Users/nicolaschapman/Documents/PrussianStringMatching/Data/'
+
+# read in map of prussia
+prussia_map = gpd.read_file(wdir+"PrussianCensus1871/GIS/1871_county_shapefile-new.shp")
+# convert to longitude and latitude for printing
+prussia_map = prussia_map.to_crs(epsg=4326)
+
+# read in merged data
+fraustadt_merged_df = pd.read_excel(wdir+"PrussianCensus1871/Fraustadt/Posen-Fraustadt-kreiskey-134-merged.xlsx")
+fraustadt_merged_df = fraustadt_merged_df[fraustadt_merged_df['lat']!=0]
+fraustadt_merged_gdf = gpd.GeoDataFrame(fraustadt_merged_df, geometry=gpd.points_from_xy(fraustadt_merged_df.lng,fraustadt_merged_df.lat))
+
+# plot prussia base map
+prussia_plot = gplt.polyplot(prussia_map, figsize=(8,4))
+gplt.pointplot(fraustadt_merged_gdf, ax=prussia_plot)
+fraustadt_plot = gplt.pointplot(fraustadt_merged_gdf)
+gplt.polyplot(prussia_map, ax=fraustadt_plot)
+plt.show()
+
