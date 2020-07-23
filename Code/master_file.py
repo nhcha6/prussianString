ed65ec4c573a1aa6bdaada39ffd99d10e58f1089
nhcha6 <nhcha6@student.monash.edu>
2020-07-23 17:33:14 +1000

    added master file

---
+++ Code/master_file.py
@@ -0,0 +1,15 @@
+from map_production_functions import *
+from merge_census_gazetter import *
+from missing_analysis import *
+
+# run the mapping code
+run_maps()
+
+# run the full merge code
+#run_full_merge()
+
+# run the statistical analysis of the missing data
+#create_missing_stats()
+
+# create histograms from the missing data
+#create_histograms()
